03a4ec8d0726a74fc74a58d2ac24efb62a687b53
ckoropoletim <143329990+ckoropoletim@users.noreply.github.com>
2023-12-06 13:28:44 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,39 @@
 
-
-def main():
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-if __name__ == "__main__":
-    main()
-    return data
-
-    for item in data:
-
-import random
-        print(f"Random Number: {item}")import random
-    for item in data:
-    return data
-        print(f"Random Number: {item}")
-
 if __name__ == "__main__":
 
 
-    main()
-def main():
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-
     data = generate_random_data()
-
-import random
-    for item in data:
 def generate_random_data():
         print(f"Random Number: {item}")
-def main():
     main()
-    return data
-if __name__ == "__main__":
-
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
 import random
-        print(f"Random Number: {item}")
-    main()
-def main():
-    return data
-def generate_random_data():
-
-if __name__ == "__main__":
     for item in data:
 
-    data = generate_random_data()
+def main():def main():
 
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+    return data
+import random
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+
+    }
+}
+        }
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+        for (int item : data) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
