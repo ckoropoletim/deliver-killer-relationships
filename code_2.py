caeceeb93720ccc1119019c713116de8826de797
ckoropoletim <143329990+ckoropoletim@users.noreply.github.com>
2023-12-09 16:48:48 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,29 @@
-main();
-        console.log("Random Number: " + item);
-    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
-
-    }
 function main() {
-    });
+    for (let i = 0; i < 10; i++) {
 
 }
     data.forEach(item => {
-    return data;
+        console.log("Random Number: " + item);
+    });
     const data = [];
-}
+    const data = generateRandomData();
 
-        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    return data;
+main();
+
 function generateRandomData() {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        }
+    public static void main(String[] args) {
+
+public class RandomDataGenerator {
+    }
+        for (int item : data) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        int[] data = new int[10];
+}
