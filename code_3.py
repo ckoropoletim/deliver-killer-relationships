77e8e1babd1ca7700707b75a6960d645a5db4e7a
ckoropoletim <143329990+ckoropoletim@users.noreply.github.com>
2023-12-14 22:28:31 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,27 @@
-
-if __name__ == "__main__":
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-def generate_random_data():
-    main()
-
-import random
     for item in data:
 
+        print(f"Random Number: {item}")
+
 def main():
+
+
+if __name__ == "__main__":
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():
+import random
+    data = generate_random_data()    main()
+
+    for item in data:
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    data = generate_random_data()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+
     return data
